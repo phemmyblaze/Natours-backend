dab2f3a66d8212201bf3bd1c711ae097b5019cd8
Akinuliola Akinbobola Oluwafemi <akinuliola_a@yahoo.com>
2023-07-07 16:52:28 +0100

    Good folder structure and arrangement

--- app.js
+++ app.js
@@ -8,9 +8,15 @@ const tourRouter = require('./routes/tourRoutes');
 const app = express();
 
 ////1) middle ware
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+
+////reading static files
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from middleware 👋');
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-const app = require('./app');
-const port = 3000;
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 
+const app = require('./app');
+
+// console.log(app.get('env'));
+// console.log(process.env);\
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}....`);
 });
+
+const x = 23;
+// x = 60;
